4fccff0551ebe02d5b82ea79b502d9bd2f3094f7
Administrator <Administrator@172.19.99.19>
2015-09-15 17:03:48 +0800

    修改

--- src/main/java/cn/liyl/demo/test/DemoTestController.java
+++ src/main/java/cn/liyl/demo/test/DemoTestController.java
@@ -1,5 +1,16 @@
 package cn.liyl.demo.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+
+@Controller
+@RequestMapping("/demo")
 public class DemoTestController {
 
+	public ModelAndView doMain(){
+		ModelAndView mv = new ModelAndView();
+		return mv;
+	}
+	
 }
