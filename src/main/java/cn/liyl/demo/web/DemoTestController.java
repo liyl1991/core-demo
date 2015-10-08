72427dea0adaeda5c49799164208076f7461e342
Administrator <Administrator@172.19.99.5>
2015-10-08 16:25:11 +0800

    修改

--- src/main/java/cn/liyl/demo/web/DemoTestController.java
+++ src/main/java/cn/liyl/demo/web/DemoTestController.java
@@ -1,19 +1,23 @@
-package cn.liyl.demo.test;
+package cn.liyl.demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/demo")
 public class DemoTestController {
 	
 	@RequestMapping("/test")
-	public ModelAndView doMain(){
-		ModelAndView mv = new ModelAndView();
-		mv.setView("index");
-		mv.addObject("msg", "Hello World!");
-		return mv;
+	public String doMain(){
+		return "jsp/index";
 	}
 	
+	@RequestMapping("/test2")
+	public ModelAndView doMain2(){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("msg", "Hello World!");
+		mv.setViewName("jsp/index");
+		return mv;
+	}
 }
